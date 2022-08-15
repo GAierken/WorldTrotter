c49219fe668b607f900a8db7fb3e762131ba56f9
GAierken <g.aierken@gmail.com>
2022-08-15 15:00:02 -0400

    first commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Guligena Aierken on 8/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
